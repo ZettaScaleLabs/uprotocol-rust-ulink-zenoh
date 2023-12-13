684eb57b54b1e66fc48d0309e13afc24c2e0831e
ChenYing Kuo <evshary@gmail.com>
2023-12-13 17:19:47 +0100

    Add the validation for UUri
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use async_trait::async_trait;
 use uprotocol_sdk::{
-    rpc::{RpcClient, RpcClientResult},
+    rpc::{RpcClient, RpcClientResult, RpcMapperError},
     transport::datamodel::UTransport,
+    uprotocol::UCode,
     uprotocol::{UAttributes, UEntity, UMessage, UPayload, UStatus, UUri},
+    uri::validator::UriValidator,
 };
 use zenoh::config::Config;
 use zenoh::prelude::r#async::*;
@@ -13,8 +15,15 @@ pub struct ULink {
 }
 
 impl ULink {
-    pub async fn new() -> anyhow::Result<ULink> {
-        let session = zenoh::open(Config::default()).res().await.unwrap();
+    /// # Errors
+    /// Will return `Err` if unable to create Zenoh session
+    pub async fn new() -> Result<ULink, UStatus> {
+        let Ok(session) = zenoh::open(Config::default()).res().await else {
+            return Err(UStatus::fail_with_code(
+                UCode::Internal,
+                "Unable to open Zenoh session",
+            ));
+        };
         Ok(ULink { _session: session })
     }
 }
@@ -22,10 +31,17 @@ impl ULink {
 #[async_trait]
 impl RpcClient for ULink {
     async fn invoke_method(
-        _topic: UUri,
+        topic: UUri,
         payload: UPayload,
         _attributes: UAttributes,
     ) -> RpcClientResult {
+        // Do the validation
+        if UriValidator::validate(&topic).is_err() {
+            return Err(RpcMapperError::UnexpectedError(String::from("Wrong UUri")));
+        }
+        // TODO: Validate UAttributes (maybe without self)
+
+        // TODO: Not implemented
         Ok(payload)
     }
 }
@@ -33,27 +49,68 @@ impl RpcClient for ULink {
 #[async_trait]
 impl UTransport for ULink {
     async fn authenticate(&self, _entity: UEntity) -> Result<(), UStatus> {
-        Err(UStatus::fail("Not implemented"))
+        // TODO: Not implemented
+        Err(UStatus::fail_with_code(
+            UCode::Unimplemented,
+            "Not implemented",
+        ))
     }
 
     async fn send(
         &self,
-        _topic: UUri,
+        topic: UUri,
         _payload: UPayload,
         _attributes: UAttributes,
     ) -> Result<(), UStatus> {
-        Err(UStatus::fail("Not implemented"))
+        // Do the validation
+        if UriValidator::validate(&topic).is_err() {
+            return Err(UStatus::fail_with_code(
+                UCode::InvalidArgument,
+                "Invalid topic",
+            ));
+        }
+        // TODO: Validate UAttributes (maybe without self)
+
+        // TODO: Not implemented
+        Err(UStatus::fail_with_code(
+            UCode::Unimplemented,
+            "Not implemented",
+        ))
     }
 
     async fn register_listener(
         &self,
-        _topic: UUri,
+        topic: UUri,
         _listener: Box<dyn Fn(UMessage) + Send + 'static>,
     ) -> Result<String, UStatus> {
-        Err(UStatus::fail("Not implemented"))
+        // Do the validation
+        if UriValidator::validate(&topic).is_err() {
+            return Err(UStatus::fail_with_code(
+                UCode::InvalidArgument,
+                "Invalid topic",
+            ));
+        }
+
+        // TODO: Not implemented
+        Err(UStatus::fail_with_code(
+            UCode::Unimplemented,
+            "Not implemented",
+        ))
     }
 
-    async fn unregister_listener(&self, _topic: UUri, _listener: &str) -> Result<(), UStatus> {
-        Err(UStatus::fail("Not implemented"))
+    async fn unregister_listener(&self, topic: UUri, _listener: &str) -> Result<(), UStatus> {
+        // Do the validation
+        if UriValidator::validate(&topic).is_err() {
+            return Err(UStatus::fail_with_code(
+                UCode::InvalidArgument,
+                "Invalid topic",
+            ));
+        }
+
+        // TODO: Not implemented
+        Err(UStatus::fail_with_code(
+            UCode::Unimplemented,
+            "Not implemented",
+        ))
     }
 }
