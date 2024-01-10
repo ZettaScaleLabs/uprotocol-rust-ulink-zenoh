0d074e49c9cc993af4593e6bece113c3b8b33d34
ChenYing Kuo <evshary@gmail.com>
2024-01-10 14:11:29 +0800

    Add RPC in integration_test.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2023 ZettaScale Technology
+// Copyright (c) 2024 ZettaScale Technology
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License 2.0 which is available at
@@ -11,16 +11,18 @@
 // Contributors:
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
 //
-use async_std::task;
+use async_std::task::{self, block_on};
+use std::sync::{Arc, Mutex};
 use std::time;
 use uprotocol_sdk::{
-    rpc::RpcServer,
+    rpc::{RpcClient, RpcServer},
     transport::builder::UAttributesBuilder,
     transport::datamodel::UTransport,
     uprotocol::{
-        Data, UCode, UEntity, UMessage, UPayload, UPayloadFormat, UPriority, UResource, UStatus,
-        UUri,
+        Data, UCode, UEntity, UMessage, UMessageType, UPayload, UPayloadFormat, UPriority,
+        UResource, UStatus, UUri, Uuid,
     },
+    uri::builder::resourcebuilder::UResourceBuilder,
 };
 use uprotocol_zenoh_rust::ULinkZenoh;
 use zenoh::config::Config;
@@ -167,4 +169,105 @@ async fn test_publish_and_subscribe() {
         .unregister_listener(uuri.clone(), &listener_string)
         .await
         .unwrap();
+}
+
+#[async_std::test]
+async fn test_rpc_server_client() {
+    let to_test_client = ULinkZenoh::new(Config::default()).await.unwrap();
+    let to_test_server = Arc::new(Mutex::new(
+        ULinkZenoh::new(Config::default()).await.unwrap(),
+    ));
+    let client_data = String::from("This is the client data");
+    let server_data = String::from("This is the server data");
+    // create uuri
+    let uuri = UUri {
+        entity: Some(UEntity {
+            name: "test_rpc.app".to_string(),
+            version_major: Some(1),
+            ..Default::default()
+        }),
+        resource: Some(UResourceBuilder::for_rpc_request(
+            Some("SimpleTest".to_string()),
+            None,
+        )),
+        ..Default::default()
+    };
+    // setup RpcServer callback
+    let to_test_server_cloned = to_test_server.clone();
+    let server_data_cmp = server_data.clone();
+    let client_data_cmp = client_data.clone();
+    let callback = move |result: Result<UMessage, UStatus>| {
+        match result {
+            Ok(msg) => {
+                let UMessage {
+                    source,
+                    attributes,
+                    payload,
+                } = msg;
+                // Get the UUri
+                let uuri = source.unwrap();
+                // Build the payload to send back
+                if let Data::Value(v) = payload.unwrap().data.unwrap() {
+                    let value = v.into_iter().map(|c| c as char).collect::<String>();
+                    assert_eq!(client_data_cmp, value);
+                } else {
+                    panic!("The message should be Data::Value type.");
+                }
+                // Get current time
+                let upayload = UPayload {
+                    length: Some(0),
+                    format: UPayloadFormat::UpayloadFormatText as i32,
+                    data: Some(Data::Value(server_data_cmp.as_bytes().to_vec())),
+                };
+                // Set the attributes type to Response
+                let mut uattributes = attributes.unwrap();
+                uattributes.set_type(UMessageType::UmessageTypeResponse);
+                // Send back result
+                block_on(
+                    to_test_server_cloned
+                        .lock()
+                        .unwrap()
+                        .send(uuri, upayload, uattributes),
+                )
+                .unwrap();
+            }
+            Err(ustatus) => {
+                panic!("Internal Error: {:?}", ustatus);
+            }
+        }
+    };
+    to_test_server
+        .lock()
+        .unwrap()
+        .register_rpc_listener(uuri.clone(), Box::new(callback))
+        .await
+        .unwrap();
+
+    // Run RpcClient
+    // create uattributes
+    // TODO: Check TTL (Should TTL map to Zenoh's timeout?)
+    // TODO: It's a little strange to create UUID by users
+    let attributes = UAttributesBuilder::request(UPriority::UpriorityCs4, uuri.clone(), 100)
+        .with_reqid(Uuid {
+            msb: 0x0000000000018000u64,
+            lsb: 0x8000000000000000u64,
+        })
+        .build();
+    // create uPayload
+    let payload = UPayload {
+        length: Some(0),
+        format: UPayloadFormat::UpayloadFormatText as i32,
+        data: Some(Data::Value(client_data.as_bytes().to_vec())),
+    };
+    let result = to_test_client
+        .invoke_method(uuri, payload, attributes)
+        .await;
+
+    // process the result
+    if let Data::Value(v) = result.unwrap().data.unwrap() {
+        let value = v.into_iter().map(|c| c as char).collect::<String>();
+        assert_eq!(server_data, value);
+    } else {
+        panic!("Failed to get result from invoke_method.");
+    }
 }
